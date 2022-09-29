5ea4640b2da0925a80add2006f9722e19b4bcc7a
nhanlt0308 <nhanlt03082000@gmail.com>
2022-09-29 07:36:54 +0700

    new checkbox

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -7,66 +7,6 @@ const Table = (props) => {
   console.log(props);
   const classes = MuiStylesButton();
 
-  const columns = [
-    {
-      name: "Name",
-    },
-    {
-      name: "Company",
-    },
-    {
-      name: "City",
-    },
-    {
-      name: "State",
-    },
-    {
-      name: "Option",
-      options: {
-        filter: true,
-        sort: false,
-        empty: true,
-        customBodyRender: (value, tableMeta, updateValue) => {
-          return (
-            <>
-              <button
-                className={`${classes.buttonDanger} ${classes.button}`}
-                onClick={(e) => {
-                  console.log(value);
-                  e.stopPropagation();
-                }}
-              >
-                Xoá{" "}
-              </button>
-              &nbsp;
-              <button
-                className={`${classes.buttonEdit} ${classes.button}`}
-                onClick={(e) => {
-                  e.stopPropagation();
-                  alert(`Clicked "Edit" for row ${tableMeta.rowIndex}`);
-                }}
-              >
-                Sửa
-              </button>
-            </>
-          );
-        },
-      },
-    },
-  ];
-
-  const options = {
-    download: false,
-    print: false,
-    filter: false,
-    sort: true,
-    rowsPerPage: 5,
-    selectableRows: "none",
-    onRowClick: (rowData, rowMeta) => {
-      console.log(rowData);
-      console.log(rowMeta);
-    },
-  };
   return (
     <ThemeProvider theme={getMuiTheme()}>
       <MUIDataTable
--- src/pages/list/GroupDevices.jsx
+++ src/pages/list/GroupDevices.jsx
@@ -1,46 +1,138 @@
 import { Button, TextField } from "@material-ui/core";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Popup from "~/components/Popup/Popup";
 import Table from "~/components/Table/Table";
-
+import Checkbox from "@material-ui/core/Checkbox";
 const GroupDevices = () => {
   const [openPopup, setOpenPopup] = useState(false);
+  const [check, setCheck] = useState();
 
-  const data = [
-    ["Joe James", "Test Corp", "Yonkers"],
-    ["John Walsh", "Test Corp", "Hartford"],
-    ["Bob Herm", "Test Corp", "Tampa"],
-    ["James Houston", "Test Corp", "Dallas"],
-    ["Joe James1", "Test Corp", "Yonkers"],
-    ["John Walsh1", "Test Corp", "Hartford"],
-    ["Bob Herm1", "Test Corp", "Tampa"],
-    ["James Houston1", "Test Corp", "Dallas"],
-    ["Joe James2", "Test Corp", "Yonkers"],
-    ["John Walsh2", "Test Corp", "Hartford"],
-    ["Bob Herm2", "Test Corp", "Tampa"],
-    ["James Houston2", "Test Corp", "Dallas"],
-  ];
+  const data =
+    // ["Joe James", "Test Corp", "Yonkers"],
+    // ["John Walsh", "Test Corp", "Hartford"],
+    // ["Bob Herm", "Test Corp", "Tampa"],
+    // ["James Houston", "Test Corp", "Dallas"],
+    // ["Joe James1", "Test Corp", "Yonkers"],
+    // ["John Walsh1", "Test Corp", "Hartford"],
+    // ["Bob Herm1", "Test Corp", "Tampa"],
+    // ["James Houston1", "Test Corp", "Dallas"],
+    // ["Joe James2", "Test Corp", "Yonkers"],
+    // ["John Walsh2", "Test Corp", "Hartford"],
+    // ["Bob Herm2", "Test Corp", "Tampa"],
+    // ["James Houston2", "Test Corp", "Dallas"],
+    [{ name: "Joe James", userCreated: "Joe James", created: "Joe James" }];
 
   const data2 = [
-    ["Joe James", "Test Corp", "Yonkers"],
-    ["John Walsh", "Test Corp", "Hartford"],
-    ["Bob Herm", "Test Corp", "Tampa"],
-    ["James Houston", "Test Corp", "Dallas"],
-    ["Joe James1", "Test Corp", "Yonkers"],
+    {
+      name: "Joe James",
+      userCreated: "Joe James",
+      created: "Joe James",
+      id: 1,
+    },
+    {
+      name: "Joe James",
+      userCreated: "Joe James",
+      created: "Joe James",
+      id: 2,
+    },
+    {
+      name: "Joe James",
+      userCreated: "Joe James",
+      created: "Joe James",
+      id: 3,
+    },
+    {
+      name: "Joe James",
+      userCreated: "Joe James",
+      created: "Joe James",
+      id: 4,
+    },
+    {
+      name: "Joe James",
+      userCreated: "Joe James",
+      created: "Joe James",
+      id: 5,
+    },
+    {
+      name: "Joe James",
+      userCreated: "Joe James",
+      created: "Joe James",
+      id: 6,
+    },
+    {
+      name: "Joe James",
+      userCreated: "Joe James",
+      created: "Joe James",
+      id: 7,
+    },
   ];
+
+  const [arrChecked, setArrChecked] = useState([]);
+
+  useEffect(() => {
+    if (check) {
+      setArrChecked(
+        data2.filter((e, index) => {
+          if (index < check) {
+            return e;
+          }
+        })
+      );
+    }
+  }, [check]);
+
+  const newData = data2.map((e, index) => {
+    return {
+      checked: { index: index, id: e.id, data: e },
+      name: e.name,
+      userCreated: e.userCreated,
+      created: e.created,
+    };
+  });
+  // ["John Walsh", "Test Corp", "Hartford"],
+  // ["Bob Herm", "Test Corp", "Tampa"],
+  // ["James Houston", "Test Corp", "Dallas"],
+  // ["Joe James1", "Test Corp", "Yonkers"],
+
   const columns = [
     {
-      name: "Tên nhóm thiết bị",
+      name: "checked",
+      label: "Checked",
+      options: {
+        filter: true,
+        sort: false,
+        empty: true,
+        customBodyRender: (value, tableMeta, updateValue) => {
+          return (
+            <Checkbox
+              checked={
+                arrChecked.findIndex((e) => {
+                  return e.id == value?.id;
+                }) != -1
+                  ? true
+                  : false
+              }
+              onChange={() => handleChange(value?.id, value?.data)}
+              inputProps={{ "aria-label": "primary checkbox" }}
+            />
+          );
+        },
+      },
     },
     {
-      name: "Người tạo ",
+      name: "name",
+      label: "Tên nhóm thiết bị",
     },
     {
-      name: "Ngày tạo",
+      name: "userCreated",
+      label: "Người tạo ",
+    },
+    {
+      name: "created",
+      label: "Ngày tạo",
     },
   ];
   const options = {
-    count: 1,
     download: false,
     print: false,
     filter: false,
@@ -52,8 +144,8 @@ const GroupDevices = () => {
   };
 
   const options2 = {
-    count: 1,
     download: false,
+    selectableRows: "none",
     print: false,
     filter: false,
     sort: true,
@@ -64,6 +156,28 @@ const GroupDevices = () => {
       setOpenPopup(true);
     },
   };
+
+  const handeChangeInput = (event) => {
+    setCheck(event.target.value);
+  };
+
+  const handleChange = (id, data) => {
+    let checkChecked = arrChecked.findIndex((e) => {
+      return e.id == id;
+    });
+    if (checkChecked != -1) {
+      setArrChecked(
+        arrChecked.filter((e) => {
+          return e.id != id;
+        })
+      );
+    } else {
+      setArrChecked((arrChecked) => [...arrChecked, data]);
+    }
+  };
+
+  console.log(arrChecked);
+
   return (
     <>
       <Table
@@ -79,10 +193,10 @@ const GroupDevices = () => {
         setOpenPopup={setOpenPopup}
       >
         <div className="popup-input">
-          <TextField label="Standard" />
+          <TextField label="Standard" onChange={handeChangeInput} />
           <button className="popup-button">Primary</button>
         </div>
-        <Table data={data2} columns={columns} options={options2} />
+        <Table data={newData} columns={columns} options={options2} />
       </Popup>
     </>
   );
